61ff4ff5bcf76c934e7eb23580bf131344e61052
LeonardoCatelan <44733148+LeonardoCatelan@users.noreply.github.com>
2019-09-11 22:34:58 -0300

    Update compiladores.cpp
    quase perto de funcionar

--- compiladores.cpp
+++ compiladores.cpp
@@ -3,15 +3,23 @@
 #include <string.h>
 
 #define tamanho 40
+#define array 15
+
+//Definindo struct de indicadores (lista de strings neste caso)
+struct reg{
+	 char ind[array];
+};
 
 main(){
 	FILE *arquivo;
 	
+	struct reg identificadores[tamanho];
 	//=======DECLARAÇÃO DE VARIÁVEIS====
-	int i = 0;
+	int i = 0, x = 0, id = 0;
 	char *palavras[tamanho];
 	char line[tamanho];
 	char *teste;
+	int erros[10];
 	const char inteiro[5] = "int";
 	const char ponto[10] = "float";
 	const char dobro[10] = "double";
@@ -40,7 +48,8 @@ main(){
 	//SETANDO I PARA 0 PARA REAPROVEITAR O MESMO NOVAMENTE
 	i = 0;
 
-//PRINTA TODAS AS PALAVRAS PARA FIM DE TESTE	
+//PRINTA TODAS AS PALAVRAS PARA FIM DE TESTE
+/*	
 	do{
 		if(i == 0){
 			printf("%s\n", palavras[i]);
@@ -51,36 +60,82 @@ main(){
 		printf("%s\n", palavras[i]);
 	}while(palavras[i] == "  ");
 	system("cls");
-	
+*/	
 	do{
-		
 		if(palavras[i][0] >= 'a' && palavras[i][0] <= 'z')
 		{
-		//Compara se existe a palavra IF em alguma linha	
-			teste = strstr(palavras[i], inteiro);
-			if(teste != NULL)
-				printf("\n[%i] INT", i+1);
-			else{
-				teste = strstr(palavras[i], ponto);
-				if(teste != NULL)
-					printf("\n[%i] FLOAT", i+1);
-				else{
-					teste = strstr(palavras[i], dobro);
-					if(teste != NULL)
-						printf("\n[%i] DOUBLE", i+1);
-					else{
-						teste = strstr(palavras[i], comentario);
-						printf("%s", teste);
+		//Compara se existe a palavra INT em alguma linha	
+			teste = strstr(palavras[i], " ");
+			if(teste != NULL){
+				erros[x] = i+1;
+				x++;
+			}else{
+				teste = strstr(palavras[i], "_");
+				if(teste != NULL){
+					erros[x] = i+1;
+					x++;
+				}else{
+					teste = strstr(palavras[i], ".");
+					if(teste != NULL){
+					erros[x] = i+1;
+					x++;
+					}else{
+						teste = strstr(palavras[i], inteiro);
 						if(teste != NULL)
-							printf("\n[%i] COMENTARIO", i+1);
+							printf("\n[%i] INT", i+1);
+						else{
+							//Compara se existe a palavra FLOAT em alguma linha
+							teste = strstr(palavras[i], ponto);
+							if(teste != NULL)
+								printf("\n[%i] FLOAT", i+1);
+							else{
+								//Compara se existe a palavra DOUBLE em alguma linha
+								teste = strstr(palavras[i], dobro);
+								if(teste != NULL)
+									printf("\n[%i] DOUBLE", i+1);
+								else{
+									teste = strstr(palavras[i], "real");
+									if(teste != NULL)
+										printf("\n[%i] REAL", i+1);
+										else{
+//											for(int j = 0; j < id; j++){
+//											if(!strcmp(palavra[i], identificador[j]);)
+//											}
+											strcpy(identificadores[id].ind, palavras[i]);
+											printf("\n[%i] IDENTIFICADOR %i", i+1, id+1);
+											id++;
+										}
+								}
+								
+							
+//									//Compara se existe a palavra // (Comentário) em alguma linha
+//									teste = strstr(palavras[i], comentario);
+//								//	printf("%s", teste);
+//									if(teste != NULL)
+//										printf("\n[%i] COMENTARIO", i+1);
+//									}
+								}
+							}
+						}
 					}
-				}
+				}	
 			}
-		}
 	i++;
-			
-	}while(palavras[i] != "  ");
+	}while(palavras[i] != NULL);
 	
+
+	printf("\nTabela de simbolos:");
+	for(i = 0; i < id; i++){
+		
+		if(identificadores[i].ind != NULL)
+			printf("\n%i", i+1);
+		printf(" - %s", identificadores[i].ind);
+	}
+	printf("\nO programa possui erros nas linhas: ");
+	for(i = 0; i < x; i++){
+		if(erros[i] != NULL)
+			printf("%i ", erros[i]);
+	}
 	
 	fclose(arquivo);
 	
